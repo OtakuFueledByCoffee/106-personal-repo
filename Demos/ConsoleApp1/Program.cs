5fceb0a97c43624c49d00519d2a45d8380808418
OtakuFueledByCoffee <jra4260@g.rit.edu>
2026-01-28 13:35:28 -0500

    Creating personal repo for IGME106
    Contains recursion demo code and starter file for list of objects PE

---
+++ Demos/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"{Program.Factorial(3)}");
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else if (n < 0)
+                return -1;
+            else if (n > 0)
+                return n * Factorial(n - 1);
+            return -2;
+        }
+    }
+}
