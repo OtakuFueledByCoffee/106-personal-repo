4a93b1907c7d9de5222be6007258a803fc3f605a
OtakuFueledByCoffee <jra4260@g.rit.edu>
2026-02-11 13:45:19 -0500

    Began Roster Class

--- PEs/PE_ListOfObjects/Roster.cs
+++ PEs/PE_ListOfObjects/Roster.cs
@@ -8,5 +8,40 @@ namespace PE_ListOfObjects
 {
     internal class Roster
     {
+        // -------------- Fields --------------
+        protected string rosterName;
+        List<Student> students;
+
+        // -------------- Properties --------------
+
+        // -------------- Constructors --------------
+        /// <summary>
+        /// Constructor for the roster of students
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="students"></param>
+        public Roster(string name, List<Student> students)
+        {
+            this.rosterName = name;
+            this.students = students;
+        }
+
+        // -------------- Methods --------------
+        public Student SearchByName(string name)
+        {
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].Name == name)
+                {
+                    return students[i];
+                }
+            }
+            return null;
+        }
+
+        public void AddStudent(Student s)
+        {
+
+        }
     }
 }
