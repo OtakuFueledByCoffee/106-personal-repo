f612d7724c02353dd122b160e9d249d304ed0a7e
OtakuFueledByCoffee <jra4260@g.rit.edu>
2026-02-16 12:57:29 -0500

    Added summary to PE_CustomStack&Queues

--- PEs/PE_CustomStack&Queues/Program.cs
+++ PEs/PE_CustomStack&Queues/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace PE_CustomStack_Queues
+﻿// Jonathan Alava
+// 2/16/2026
+// Practicing Stacks and Queues by making a custom stack
+namespace PE_CustomStack_Queues
 {
     internal class Program
     {
