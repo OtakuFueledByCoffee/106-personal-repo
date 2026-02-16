90862db39830e2216ccd9a55d8be2a44dd5ad1ed
OtakuFueledByCoffee <jra4260@g.rit.edu>
2026-02-16 12:55:21 -0500

    Created PE_CustomStack&Queues

---
+++ PEs/PE_CustomStack&Queues/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PE_CustomStack_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
