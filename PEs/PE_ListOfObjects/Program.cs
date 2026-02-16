836ad9e38380ed81a2df84ef337c9d9a9e88f39e
OtakuFueledByCoffee <jra4260@g.rit.edu>
2026-02-16 12:53:07 -0500

    Completed roster class

--- PEs/PE_ListOfObjects/Program.cs
+++ PEs/PE_ListOfObjects/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Jonathan Alava
 // 1/28/2026
 // Practicing how a list of objects work
+// It's 2/14/2026 now god I'm behind but I'm finishing it wooooo!
 namespace PE_ListOfObjects
 {
     internal class Program
--- PEs/PE_ListOfObjects/Roster.cs
+++ PEs/PE_ListOfObjects/Roster.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -36,12 +37,104 @@ namespace PE_ListOfObjects
                     return students[i];
                 }
             }
-            return null;
+            return null!;
         }
 
+        /// <summary>
+        /// Method to add a student to the roster without user input
+        /// </summary>
+        /// <param name="s"></param>
         public void AddStudent(Student s)
         {
+            // Checkign if student name already exists
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].Name == s.Name)
+                {
+                    students.Add(s);
 
+                    // Print confirmation & return to end
+                    Console.WriteLine($"Added {s.Name} to the roster.");
+
+                    return;
+                }
+            }
+
+            // Confirmation message & return to end
+            Console.WriteLine($"Unable to add {s.Name} to roster: {s.Name} already exists.");
+
+            return;
+        }
+
+        /// <summary>
+        /// Overload method (not really, the opposite technically) to add a custom student to the rester
+        /// by prompting user input
+        /// </summary>
+        /// <returns></returns>
+        public Student AddStudent()
+        {
+            // Declaring values
+            string name;
+            string major;
+            string yearAsString;
+            int year;
+            Student student;
+
+            // Ask user for information
+            Console.Write("Full Name: ");
+            name = Console.ReadLine()!;
+            Console.Write("Major: ");
+            major = Console.ReadLine()!;
+            Console.WriteLine("Year Level: ");
+            yearAsString = Console.ReadLine()!;
+
+            // Checking if year inputted is a valid integer
+            while (!int.TryParse(yearAsString, out year))
+            {
+                Console.Write("Invalid input: Integers only. ");
+                yearAsString = Console.ReadLine()!;
+            }
+
+            // Create new Student object
+            student = new Student(name, major, year);
+
+            // Check if student exists in roster
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].Name == student.Name)
+                {
+                    // Adding student to roster
+                    students.Add(student);
+
+                    // Print confirmation 
+                    Console.WriteLine($"Added {student.Name} to to the roster.");
+
+                    // Return student object
+                    return student;
+                }
+            }
+
+            // Print confirmation if student is already in roster
+            Console.WriteLine($"Unable to add {student.Name} to roster: {student.Name} already exists.");
+
+            // Return null
+            return null!;
+        }
+
+        /// <summary>
+        /// Method to display the roster of students
+        /// </summary>
+        public void DisplayRoster()
+        {
+            // Printing roster information
+            Console.WriteLine($"Roster: {students.Count} students");
+            
+            // For loop to print all student information from list
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine($"\tStudent #{i+1}:");
+                students[i].ToString();
+            }
         }
     }
 }
